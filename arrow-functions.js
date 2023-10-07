c12e4e9b9cd36c69dbf861c304a4ad5c529f3d20
Grace How <gracehow8702@gmail.com>
2023-10-07 07:24:57 +0000

    Arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,45 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers(a, b) {
+//     //code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3 , 5);
+// console.log(sum);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    //code block
+    return a + b;
+}
+let sum = addTwoNumbers(3 , 5);
+console.log(sum);
+
 
 // Single Line Arrow Function With Parameters
 
+// const addTwoNumbers2 = (a, b) => (a + b);
+// const addTwoNumbers2 = (a, b) => (a + b);
+// let sum2 = addTwoNumbers2(6, 4);
+// console.log(sum2);
+
 // Implicit Returns
 
+const saySonething = message => console.log(message)
+saySonething('Hello there!!')
+
+const sayHello = () => console.log('hello');
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `
+    <p>
+        This is a multiline string!
+    </p>
+    `
+)
+console.log(returnMultipleLines())
